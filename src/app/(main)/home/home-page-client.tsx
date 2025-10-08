67a0748c0444cd7b0e584cfbe8e2e3f3acf55681
santyasm <yyasant5@gmail.com>
2025-10-07 22:19:16 -0300

    Refactor HomePageClient and MainLayout components for improved layout consistency; adjust spacing and structure for better mobile responsiveness and user experience.

--- src/app/(main)/home/home-page-client.tsx
+++ src/app/(main)/home/home-page-client.tsx
@@ -5,25 +5,27 @@ import { RightSidebar } from "@/components/right-side-bar";
 
 export default function HomePageClient() {
   return (
+
     <div
       className="
-        flex w-full justify-center
-        mx-auto
-        pb-12
+      flex w-full justify-center
+      mx-auto
+      pb-12
       "
     >
       <MainFeed />
 
       <aside
         className="
-          hidden xl:block
-          w-[310px]
-          lg:w-[23vw]
-          flex-shrink-0
+        hidden xl:block
+        w-[310px]
+        lg:w-[23vw]
+        flex-shrink-0
         "
       >
         <RightSidebar />
       </aside>
     </div>
+
   );
 }
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,25 +2,26 @@ import { FloatingNavBar } from "@/components/floating-navbar";
 import { LeftSidebar } from "@/components/left-side-bar";
 
 export default function MainLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <div className="min-h-screen flex justify-center bg-background text-foreground">
-      <div className="fixed ">
-        <LeftSidebar />
-      </div>
+    return (
+        <>
+            <div className="sm:hidden flex">
+                <FloatingNavBar />
+            </div>
+            <div className="min-h-screen flex justify-center bg-background text-foreground">
+                <div className="fixed ">
+                    <LeftSidebar />
+                </div>
 
-      <div className="sm:hidden flex">
-        <FloatingNavBar />
-      </div>
+                <div className="sm:w-[96px] md:w-[100px] lg:w-[250px] flex-shrink-0 hidden sm:block" />
 
-      <div className="sm:w-[96px] md:w-[100px] lg:w-[250px] flex-shrink-0 hidden sm:block" />
-
-      <main className="flex-1 flex justify-center pb-[90px] sm:pb-0">
-        {children}
-      </main>
-    </div>
-  );
+                <main className="flex-1 flex justify-center pb-[90px] sm:pb-0">
+                    {children}
+                </main>
+            </div>
+        </>
+    );
 }
